4149fae5e0351a2a0dd9dcbfd10d1b0fc4b038b8
Venkateswara Raju Datla <dvrajudatla1995@gmail.com>
2018-06-23 16:59:14 -0700

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
-
+var {ObjectID}=require('mongodb');
 var {mongoose}=require('./db/mongoose.js');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
@@ -24,6 +24,22 @@ app.get('/todos',(req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id',(req,res)=>{
+var id=req.params.id;
+if(!ObjectID.isValid(id))
+{
+  console.log('Not valid');
+  return res.status(404).send();
+}
+Todo.findById(id).then((todo)=>{
+if(!todo)
+{
+  return res.status(404).send();
+}
+ res.send({todo});
+});
 });
 
 app.listen(3000,()=>{
