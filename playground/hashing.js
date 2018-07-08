9ca13c164b4584f2719066d3508928a9589fcd80
Venkateswara Raju Datla <dvrajudatla1995@gmail.com>
2018-07-07 17:33:58 -0700

    Hash User password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,20 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-var data={
+var password="rajuabc";
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  });
+});
+
+var hashpassword='$2a$10$YPtPXQQFvqv4I.vtOFnbO.CBR4IdpZGgmNKe.2OBGw717f2sc1Req';
+bcrypt.compare(password,hashpassword,(err,res)=>{
+  console.log(res);
+})
+
+/*var data={
   id:10
 };
 
@@ -10,7 +23,7 @@ console.log('token ',token);
 
 var decoded=jwt.verify(token,'123abc');
 console.log('decoded ',decoded);
-/* var message="Token is for user 3";
+ var message="Token is for user 3";
 var hash=SHA256(message).toString();
 
 console.log(`Mesaage is ${message}`);
